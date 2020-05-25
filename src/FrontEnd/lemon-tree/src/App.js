e69861c098e2d55a0fb00be5da3dd95910b05cfd
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2020-05-25 08:16:59 +0200

    app is a component now

--- src/FrontEnd/lemon-tree/src/App.js
+++ src/FrontEnd/lemon-tree/src/App.js
@@ -2,20 +2,29 @@ import React from 'react';
 import './App.css';
 import NumericGauge from './NumericGauge.js';
 
-function App() {
-  return (
-    <>
-    <div className="App">
-      <header className="App-header">
-        <NumericGauge value="Temp: 18º"/>
-        <NumericGauge value="Hum: 20%"/>
-        <NumericGauge value="Sun: 40"/>
-        <img src={process.env.PUBLIC_URL + '/lemontree.png'} className="LemonTree-logo" alt="logo" />
-      </header>
-    </div>
-    
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      temperature: 18,
+      humidity: 21,
+      sun: 37,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <NumericGauge value={`Temp: ${this.state.temperature}º`}/>
+          <NumericGauge value={`Hum: ${this.state.humidity}%`}/>
+          <NumericGauge value={`Sun: ${this.state.sun}`}/>
+          <img src={process.env.PUBLIC_URL + '/lemontree.png'} className="LemonTree-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  };
 }
 
 export default App;
